c89cd6dc1701ee0530529d1ef6dd1c25e7b88a1b
hanerx <zhangyu199946@gmail.com>
2022-09-04 01:13:24 +0800

    grid card

--- lib/view/components/grid_card.dart
+++ lib/view/components/grid_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class GridCard extends StatelessWidget{
+class GridCard extends StatelessWidget {
   const GridCard({super.key});
 
   @override
@@ -12,24 +12,32 @@ class GridCard extends StatelessWidget{
         children: [
           Row(
             children: [
-              Text("Title"),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Title",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
               Expanded(child: SizedBox()),
-              IconButton(onPressed: (){}, icon: Icon(Icons.arrow_forward_ios))
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 20,
+                    color: Theme.of(context).disabledColor,
+                  ))
             ],
           ),
           GridView(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3),
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
             shrinkWrap: true,
-            children: [
-              Card(),
-              Card(),
-              Card()
-            ],
+            children: [Card(), Card(), Card()],
           )
         ],
       ),
     );
   }
-
 }
